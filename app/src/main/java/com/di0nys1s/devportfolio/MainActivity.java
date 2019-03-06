c733a6e83105f00275f2775a8cd7c833e7662467
Burak Seyhan <seyhanburak9@gmail.com>
2019-03-06 06:07:22 -0500

    comment2224444

--- app/src/main/java/com/di0nys1s/devportfolio/MainActivity.java
+++ app/src/main/java/com/di0nys1s/devportfolio/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
 
     @Override
-    //mrrffff
+    //burak
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
